05f3cfc0b921d1f773638cda8a0bcf91fe385006
wiktor.kobiela <wuch.mail@gmail.com>
2022-05-22 23:20:23 +0200

    Small fix for handling weird prices options

--- libs/func.py
+++ libs/func.py
@@ -13,7 +13,8 @@ def create_elements_list(items_list: list, elem_to_cut, attribute):
 
 def create_dictionary(given_dictionary: dict, given_city, max_value):
     for key, value in list(given_dictionary.items()):
-        if int(value[0][:-3]) > int(max_value):  # max value entered
+        int_price_value = re.search(r'(.*?)zł', value[0]).group(1)
+        if int(int_price_value.replace(" ", "")) > int(max_value):  # max value entered
             # print(value[0][:-3])
             del given_dictionary[key]
         elif f"{given_city}, ".lower() not in value[1].lower():  # city entered
